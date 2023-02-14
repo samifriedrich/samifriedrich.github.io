197a5eb0acecf4dbcf34635766e058074330e8c1
Sami Friedrich <sami.r.friedrich@gmail.com>
2023-02-14 02:39:06 +0100

    Major restyling of color, breakpoints, and responsivity

--- script/script.js
+++ script/script.js
@@ -1,18 +1,3 @@
-// add class navbarDark on navbar scroll
-const header = document.querySelector('.navbar');
-window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
-}
+
 // close hamburger navbar after clicking a link
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
+$('.navbar-nav>li>a').on('click', function () { $('.navbar-collapse').collapse('hide'); });
